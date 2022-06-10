5463a29ba608a8bede4eeb7844465e3436937185
Lisa DeSpain <lisamdespain@gmail.com>
2022-06-10 13:48:37 -0500

    actions are passing

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,40 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model')
+
+function verifyActionId(req,res, next){
+    Actions.get(req.params.id)
+    .then(result =>{
+        if (result == null){
+            res.status(404).json({message: 'No action with that ID exists.'})
+            return;
+        }
+        req.action = result;
+        next();
+})
+}
+
+function verifyAction(req, res, next){
+    let { notes, description, project_id } = req.body
+    if (notes == null || notes.trim() =='' || description == null || description.trim() == '' || project_id == null){
+        res.status(400).json({message: 'The notes, description,and project id are required'})
+        return;
+    }
+    req.newAction = {notes, description, project_id};
+    next();
+}
+
+function verifyCompleted(req, res, next){
+    let { completed } = req.body
+    if (completed == null){
+        res.status(400).json({message: 'Name, description, and completed are required'})
+        return;
+    }
+    req.newAction = req.body;
+    next();
+}
+
+module.exports = {
+    verifyActionId,
+    verifyAction,
+    verifyCompleted
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,8 +3,47 @@ const express = require('express');
 
 const Actions = require('./actions-model.js');
 // bring in middleware 
-
+const { validateProjectId } = require('../projects/projects-middleware')
+const { verifyActionId, verifyAction } = require('./actions-middlware')
 const router = express.Router();
 
+router.get('/', (req, res) =>{
+    Actions.get()
+    .then(actionList =>{
+        res.status(200).json(actionList)
+    }).catch(error =>{
+        res.status(500).json({message: 'There is a problem with the server'})
+    })
+})
 
+router.get('/:id', verifyActionId, (req, res) =>{
+    res.status(200).json(req.action);
+})
+
+router.post('/', verifyAction, validateProjectId, (req, res) =>{
+    Actions.insert(req.newAction)
+    .then(action=>{
+        res.status(201).json(action)
+    }).catch(error =>{
+        res.status(500).json({message: 'There is a problem with the server'})
+    })
+})
+
+router.put('/:id', verifyActionId, verifyAction, (req, res)=>{
+    Actions.update(req.params.id, req.body)
+    .then(updatedAction =>{
+        res.status(200).json(updatedAction)
+    }).catch(error =>{
+        res.status(500).json({ message: 'there was an issue with the server'})
+      })
+})
+
+router.delete('/:id', verifyActionId, (req, res) =>{
+    Actions.remove(req.params.id)
+    .then(result =>{
+        res.status(200).json({message: 'The action was deleted'});
+    }).catch(error =>{
+        res.status(500).json({ message: 'there was an issue with the server'})
+      })
+})
 module.exports = router;
